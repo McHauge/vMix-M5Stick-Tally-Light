73bc60bb4371bbe8718cdc1b40b3019c508788de
McHauge <mc-hauge@hotmail.com>
2021-04-01 22:43:06 +0200

    added battery indicator to home screen

--- src/a_GLOBAL/h_APSCREEN.ino
+++ src/a_GLOBAL/h_APSCREEN.ino
@@ -4,14 +4,14 @@ void showAPScreen()
     Serial.println("Showing Access Point Screen");
     screen = 4;
     
-    M5.Lcd.println("Unable to connect to WiFi");
+    renderBatteryLevel();
     M5.Lcd.println();
+    M5.Lcd.println("Unable to connect to WiFi");
     M5.Lcd.println("Please connect to:");
     M5.Lcd.println();
     M5.Lcd.println("SSID: vMix-M5Stick-Tally");
     M5.Lcd.println("Pwd: 12345678");
     M5.Lcd.println();
+    M5.Lcd.println("To configure the settings");
     M5.Lcd.println("Open http://192.168.4.1");
-    M5.Lcd.println("in a browser to");
-    M5.Lcd.println("configure the settings");
 }
